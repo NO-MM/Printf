8ca4f6b7ddd05cd595dcb3392b1f86e2667ef2af
root <kingfuldanso@gmail.com>
2024-07-30 07:14:37 -0700

    Header file

---
+++ main.h
@@ -0,0 +1,20 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdio>
+#include <unistd.h>
+
+int _printf(const char *format, ...);
+
+/*These are functions to print chars and strings */
+int print_char(va_list types, char buffer[],
+	int flags, int width, int precision, int size);
+int print_string(va_list types, char buffer[],
+	int flags, int width, int precision, int size);
+int print_percent(va_list types, char buffer[],
+	int flags, int width, int precision, int size);
+
+int handle_print(const char *fmt, int *i,
+va_list list, char buffer[], int flags, int width, int precision, int size);
+
+
+#endif
