d72fd2066b5e77008bb198bfc149386ab3d34200
NO-MM <khuzwanohnoh@gmail.com>
2024-07-30 07:50:17 -0700

    Main.h updates

--- main.h
+++ main.h
@@ -12,6 +12,15 @@ int print_string(va_list types, char buffer[],
 	int flags, int width, int precision, int size);
 int print_percent(va_list types, char buffer[],
 	int flags, int width, int precision, int size);
+int print_int(va_list types, char buffer[],
+        int flags, int width, int precision, int size);
+int print_binary(va_list types, char buffer[],
+        int flags, int width, int precision, int size);
+int print_int(va_list types, char buffer[],
+        int flags, int width, int precision, int size);
+int print_binary(va_list types, char buffer[],
+        int flags, int width, int precision, int size);
+
 
 int handle_print(const char *fmt, int *i,
 va_list list, char buffer[], int flags, int width, int precision, int size);
