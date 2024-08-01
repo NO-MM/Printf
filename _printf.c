f16379bea28fa521bc5d3fe6acdc426daab29b0a
NO-MM <khuzwanohnoh@gmail.com>
2024-07-31 22:19:51 -0700

    printf

--- _printf.c
+++ _printf.c
@@ -15,17 +15,16 @@ if (format == NULL)
  return (-1);
 va_start(list, format);
 for (i = 0; format && format[i] != '\0'; i++)
- {
+{
 if (format[i] != '%')
 {
 buffer[buff_ind++] = format[i];
 if (buff_ind == BUFF_SIZE)
 print_buffer(buffer, &buff_ind);
-/* write(1, &format[i], 1);*/
 printed_chars++;
- }
+}
 else
- {
+{
 print_buffer(buffer, &buff_ind);
 flags = get_flags(format, &i);
 width = get_width(format, &i, list);
@@ -50,7 +49,7 @@ return (printed_chars);
  */
 void print_buffer(char buffer[], int *buff_ind)
 {
- if (*buff_ind > 0)
+if (*buff_ind > 0)
 write(1, &buffer[0], *buff_ind);
 *buff_ind = 0;
 }
